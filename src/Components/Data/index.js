2f4e6e2745271649b2154722696af4f3332d3f08
salman <106161169+pathan8141@users.noreply.github.com>
2022-12-11 13:53:17 +0530

    Login with localstorage

---
+++ src/Components/Data/index.js
@@ -0,0 +1,12 @@
+const employeesData=[
+    {
+        id: 1,
+    firstName: 'Salman',
+    lastName: 'Pathan',
+    email: 'pathan@example.com',
+    salary: '50000',
+    date: '2019-04-11',
+    }
+]
+
+export {employeesData};
